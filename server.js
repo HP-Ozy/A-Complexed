5b0877b4193ac7de906fa41fa08903173f54e78d
OsvaldoRoscani <roscaniuni1@gmail.com>
2023-11-30 13:05:28 +0100

    k

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var server = express();
+
+server.use()
+
+
+server.listen(8000, function check(err){
+    if(err){
+
+        console.log("error")
+
+    }else{
+        console.log('connected')
+    }
+});
+
+ 
